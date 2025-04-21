349156d44a7471c299fe6f2535395d612787ffe4
WIN-R1AHMU6F5RV\Hyper-v <developer@akqa.com>
2025-04-21 17:53:54 +1200

    Minor improvements

--- apps/nextjs-demo/src/Navigation.tsx
+++ apps/nextjs-demo/src/Navigation.tsx
@@ -7,19 +7,9 @@ const publicUrl = config.publicUrl;
 const Navigation = (): JSX.Element => (
   <div>
     <nav>
-      <ul>
-        <li>
           <a href="https://sitecore.com">
             <img src={`${publicUrl}/sc_logo.svg`} alt="Sitecore" />
-          </a>
-        </li>
-        <li>
-          <a href="https://jss.sitecore.com">JSS Documentation</a>
-        </li>
-        <li>
-          <a href="https://github.com/Sitecore/jss">JSS Repository</a>
-        </li>
-      </ul>
+          </a>      
     </nav>
   </div>
 );
--- apps/nextjs-demo/src/components/ChatBotWidget.tsx
+++ apps/nextjs-demo/src/components/ChatBotWidget.tsx
@@ -1,9 +1,67 @@
 import ChatBot from "react-chatbotify";
 
 const ChatBotWidget = () => {
-  return (
-    <ChatBot/>
-  );
+	let hasError = false;
+
+	// example openai conversation
+	// you can replace with other LLMs such as Google Gemini
+	const call_server = async (params: any) : Promise<void> => {
+
+        console.log('call_server', params);
+
+		try {
+			
+			// for streaming responses in parts (real-time), refer to real-time stream example
+			let userInput = "That's what you said: " + params.userInput.trim();
+
+			await params.injectMessage(userInput);
+        } catch (error) {
+			await params.injectMessage("Unable to load model, is your API Key valid?");
+			hasError = true;
+		}
+	}
+	const flow={
+		start: {
+			message: "Hello and welcome to chatbot!",
+			path: "loop"			
+		},		
+		loop: {
+			message: async (params:any) => {
+				await call_server(params);
+			},
+			path: () => {
+				if (hasError) {
+					return "start"
+				}
+				return "loop"
+			}
+		}
+	}
+
+    const chatBotSettings = {
+        general: {
+            embedded: false
+        }, 
+        fileAttachment: {
+            disabled: true
+        },
+        chatHistory: {
+            storageKey: "example_llm_conversation"
+        },
+        tooltip: {
+            mode: "NEVER",
+            title: "Talk to Sitecore chatbot"
+        },
+        header: {
+            showAvatar: false,
+            title: "Peter The Bot",
+
+        }
+    }
+
+	return (
+		<ChatBot settings={chatBotSettings} flow={flow}/>
+	);
 };
 
 export default ChatBotWidget;
