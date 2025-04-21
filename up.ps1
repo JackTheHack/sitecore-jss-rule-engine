b534c74e9e6e309e9e56ea4ecaf5a513f4c289d0
WIN-R1AHMU6F5RV\Hyper-v <developer@akqa.com>
2025-04-21 12:05:42 +1200

    Adding workflow templates

--- up.ps1
+++ up.ps1
@@ -39,6 +39,10 @@ if (-not $envCheck) {
 Write-Host "Keeping XM Cloud base image up to date" -ForegroundColor Green
 docker pull "$($sitecoreDockerRegistry)sitecore-xmcloud-cm:$($sitecoreVersion)"
 
+$xmcloudDockerToolsImage = ($envContent | Where-Object { $_ -imatch "^TOOLS_IMAGE=.+" }).Split("=")[1]
+Write-Host "Keeping XM Cloud Tools image up to date" -ForegroundColor Green
+docker pull "$($xmcloudDockerToolsImage):$($sitecoreVersion)"
+
 # Build all containers in the Sitecore instance, forcing a pull of latest base containers
 Write-Host "Building containers..." -ForegroundColor Green
 docker compose build
