b954ac9616319263d823b663c58d5bb54f8e1718
WIN-R1AHMU6F5RV\Hyper-v <developer@akqa.com>
2025-07-27 16:45:21 +1200

    Fix build

--- packages/sitecore-jss-rule-engine-chat/index.ts
+++ packages/sitecore-jss-rule-engine-chat/index.ts
@@ -3,5 +3,10 @@ export type { IChatActionCommand } from './src/client/chatActionFactory'
 export {registerChatActions} from './src/registerChatActions'
 export {registerChatCommands} from './src/registerChatCommands'
 export {registerChatRuleEngine} from './src/registerChatRuleEngine'
-export {Action, ErrorResponse, Metadata, SuccessResponse} from './src/client/types'
+export type {
+    Action,
+    ErrorResponse,
+    Metadata,
+    SuccessResponse
+  } from './src/client/types'
 export { handleChatMessage } from './src/handlers/chatMessageHandler'
