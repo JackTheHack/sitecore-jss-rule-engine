0d00519c333cf6d40ed740de603a63dc567d748c
WIN-R1AHMU6F5RV\Hyper-v <developer@akqa.com>
2025-07-27 17:10:18 +1200

    Fixing Vercel build

--- packages/sitecore-jss-rule-engine/index.ts
+++ packages/sitecore-jss-rule-engine/index.ts
@@ -2,6 +2,11 @@ import { JssRuleEngine, IJssRuleEngine} from './src/ruleEngine';
 import { RuleData, RuleActionData, ClientCommand, RuleEngineContext, RuleConditionData, ConditionFunctionDefinition, ActionFunctionDefinition, OperatorFunctionDefinition } from './src/types/ruleEngine';
 import {RuleEngineSessionContext} from './src/types/ruleEngineSessionContext'
 
+declare global {
+    var EdgeRuntime: string;
+    var JssEngine: JssRuleEngine;
+}
+
 var isEdgeRuntime = typeof global.EdgeRuntime == 'string';
 
 if(!isEdgeRuntime && !global.JssEngine)
